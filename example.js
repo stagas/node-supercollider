ceb364fbaa3de79da0eb6d2eb8346c758b0adaa9
George Stagas <gstagas@gmail.com>
2011-05-06 20:06:48 +0300

    First commit

---
+++ example.js
@@ -0,0 +1,29 @@
+var sc = require('supercollider')()
+
+// listen on all messages
+sc.on('message', function(message, address) {
+  console.log('message:', message, address)
+})
+
+// talk to server
+sc.send('/dumpOSC', 1, dump)
+sc.send('/notify', 1, dump)
+sc.send('/status', dump)
+
+// synthdef sine
+var sine
+sc.send('/s_new', 'sine', (sine = sc.nextNodeId()), 1, 1, dump)
+
+// stop sound after 2 seconds
+setTimeout(function() {
+  sc.send('/n_free', sine, dump)
+  process.exit(0)
+}, 2000)
+
+// debug function
+function dump() {
+  var args = [].slice.call(arguments)
+  if (args[0]) console.log('error:', args[0])
+  args[0] = 'reply:'
+  console.log.apply(console, args)
+}
---
+++ supercollider.js
@@ -0,0 +1,50 @@
+var util = require('util')
+  , path = require('path')
+  , osc = require('node-osc')
+  , EventEmitter = require('events').EventEmitter
+
+var SuperCollider = module.exports = function(port, host) {
+  if (!(this instanceof SuperCollider)) return new SuperCollider(port, host)
+  var self = this
+
+  EventEmitter.call(this)
+  
+  this.port = port || 57110
+  this.clientPort = this.clientOriginalPort = this.port + 30
+  this.host = host || '127.0.0.1'
+  this._nextId = 1000
+}
+
+util.inherits(SuperCollider, EventEmitter)
+
+SuperCollider.prototype._createClient = function(port, rr, host) {
+  console.log(port, rr, host)
+  return new osc.Client(port, rr, host, true)
+}
+
+SuperCollider.prototype.roundRobinPort = function() {
+  if (this.clientPort > this.clientOriginalPort + 50) this.clientPort = this.clientOriginalPort
+  return this.clientPort++
+}
+
+SuperCollider.prototype.send = function() {
+  var self = this
+    , args = [].slice.call(arguments)
+    , cb
+  if ('function' === typeof args[args.length - 1]) {
+    cb = args.pop()
+  }
+  var scc = this._createClient(this.port, this.roundRobinPort(), this.host)
+  cb && scc._sock.once('message', function(message, address) {
+    var messageString = message.toString()
+    self.emit('message', messageString, address)
+    cb(null, messageString, address)
+  })
+  scc.send.apply(scc, args)  
+  return scc
+}
+
+// Get next node id
+SuperCollider.prototype.nextNodeId = function() {
+  return this._nextId++
+}
